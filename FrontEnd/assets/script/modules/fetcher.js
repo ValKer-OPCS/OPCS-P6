d8a28d82131b1c4700cadc60e3de51305a1f9618
ValKer-OPCS <icyfry@gmail.com>
2025-09-02 11:49:21 +0200

    feat : Add POST login request to fetcher module
    Introduces a new postTo function for handling login requests via POST to the API. The function manages authentication, error handling, and token storage in sessionStorage, redirecting on success. Also refactors getFrom to use 'endpoint' instead of 'resource'.

--- FrontEnd/assets/script/modules/fetcher.js
+++ FrontEnd/assets/script/modules/fetcher.js
@@ -1,3 +1,7 @@
+import  {loginFailed} from "../modules/login.js";
+
+const baseUrl = 'http://127.0.0.1:5678/api/';
+
 /**
  * Fetches data from the specified API resource.
  *
@@ -5,10 +9,10 @@
  * @param {string} resource - The API resource endpoint to fetch data from.
  * @returns {Promise<any>} The data returned from the API.
  */
-export async function getFrom(resource) {
-    const baseUrl = 'http://127.0.0.1:5678/api/';
+export async function getFrom(endpoint) {
+
     try {
-        const response = await fetch(baseUrl + resource);
+        const response = await fetch(baseUrl + endpoint);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -17,5 +21,36 @@ export async function getFrom(resource) {
     } catch (error) {
         console.error('Fetch error:', error);
         throw error;
+    }
+}
+
+
+export async function postTo(endpoint,form) {
+    try {
+        const response = await fetch(baseUrl + endpoint, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: form.email.value,
+                password: form.password.value,
+            }),
+        });
+
+        if (!response.ok) {
+            loginFailed()
+            throw new Error("Email ou mot de passe erronés");
+            
+
+        }
+
+        const data = await response.json();
+        sessionStorage.setItem("token", data.token);
+        window.location.replace("../index.html");
+    } catch (error) {
+        alert(error.message || "Une erreur est survenue lors de la connexion");
+        console.error("Erreur login:", error);
     }
 }
