a03d1a8024eb2e6a04f41d26709bfa5b0ed81f53
ValKer-OPCS <icyfry@gmail.com>
2025-09-05 10:07:54 +0200

    docs : Add JSDoc comments to utility functions
    Added detailed JSDoc comments to the clearHTMLElement, smoothScrollOnLoad, and clearSessionStorage functions in utils.js to improve code documentation and maintainability.

--- FrontEnd/assets/script/modules/utils.js
+++ FrontEnd/assets/script/modules/utils.js
@@ -1,3 +1,8 @@
+/**
+ * Clears the inner HTML content of the given element specified by the CSS selector.
+ *
+ * @param {string} elementToClear - A CSS selector string representing the element to be cleared.
+ */
 export function clearHTMLElement(elementToClear){
     const element = document.querySelector(elementToClear);
     if (element) {
@@ -6,6 +11,12 @@ export function clearHTMLElement(elementToClear){
         console.warn(`Aucun élément trouvé pour le sélecteur "${elementToClear}"`);
     }
 }
+
+
+/**
+ * Adds an event listener to the window object to perform a smooth scroll on page load if there is a hash in the URL.
+ * When the window is loaded, the function finds the target element corresponding to the hash and scrolls to it smoothly.
+ */
 export function smoothScrollOnLoad(){
     window.addEventListener("load", () => {
   if (window.location.hash) {
@@ -19,6 +30,11 @@ export function smoothScrollOnLoad(){
 });
 }
 
+/**
+ * Removes the specified item from the session storage.
+ *
+ * @param {string} item - The key of the item to be removed from session storage.
+ */
 export function clearSessionStorage(item) {
   sessionStorage.removeItem(item);
 }
