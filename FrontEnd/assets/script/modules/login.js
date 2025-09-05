42f37aaef0d6a24d9b056cbfcca381ef35974029
ValKer-OPCS <icyfry@gmail.com>
2025-09-05 10:08:16 +0200

    feat : Improve login error handling and add documentation
    Enhanced the login module by integrating the handleHttpErrors utility for more descriptive error messages on failed login attempts. Added JSDoc comments to improve code readability and maintainability.

--- FrontEnd/assets/script/modules/login.js
+++ FrontEnd/assets/script/modules/login.js
@@ -1,9 +1,27 @@
 import  {postToLogin} from "../modules/fetcher.js";
+import { handleHttpErrors } from "./httpErrorHandler.js";
 
+
+/**
+ * Initializes the login process by setting up the login submission event listener.
+ *
+ * @function loginInit
+ */
 function loginInit(){
   loginSubmit()
 }
 
+
+/**
+ * Attaches a submit event listener to the document that handles login form submissions.
+ * Prevents the default form submission behavior, retrieves the user's email and password,
+ * and attempts to log in via the postToLogin function. If the login is successful, the user is redirected
+ * to the homepage.
+ *
+ * @async
+ * @function loginSubmit
+ * @returns {Promise<void>} A promise that resolves once the login submission process is complete.
+ */
 async function loginSubmit(){
   document.addEventListener("submit", async (submitButton) => {
   submitButton.preventDefault();
@@ -21,11 +39,20 @@ async function loginSubmit(){
 });}
 
 
+/**
+ * Handles a failed login attempt by processing the error response and displaying an appropriate error message.
+ * The error message is extracted using the handleHttpErrors function and then displayed in the
+ * designated error field element.
+ *
+ * @function loginFailed
+ * @param {Object} response - The HTTP error response object received from the login attempt.
+ */
+export function loginFailed(response) {
+  const errorField = document.getElementById("errorField");
 
-export function loginFailed() {
-  const errorField = document.getElementById('errorField');
   if (errorField) {
-    errorField.innerText = 'Email ou mot de passe erronés';
+    const message = handleHttpErrors(response);
+    errorField.innerText = message;
   }
 }
 
